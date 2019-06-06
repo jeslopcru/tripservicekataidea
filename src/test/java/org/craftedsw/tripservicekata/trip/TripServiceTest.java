d6a57cad9d7d5d46bc6aae33903b5d39a54dd1cf
jlopez <jlopez@qashops>
2019-06-06 09:55:18 +0200

    All is working

--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,12 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.junit.Test;
+
 public class TripServiceTest {
+
+    @Test
+    public void all_is_working() {
+        assert (true);
+    }
 
 }
