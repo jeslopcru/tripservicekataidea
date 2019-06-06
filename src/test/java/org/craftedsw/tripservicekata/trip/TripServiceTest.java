2af3a09ec932027511ae66a910033db1e7a5be5d
jlopez <jlopez@qashops>
2019-06-06 10:30:13 +0200

    should_return_any_trips_when_user_is_logged_and_are_not_friends

--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -4,25 +4,46 @@ import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * TODO LIST:
  * + No hay un usuario loggeado
  * - El usuario logeado y el objetivo son amigos
- * - El usuario logeado y el objetivo no son amigos
+ * + El usuario logeado y el objetivo no son amigos
  * - El usuario logeado y el objetivo son amigo y el objetivo tiene viajes
  */
 public class TripServiceTest {
 
     @Test(expected = UserNotLoggedInException.class)
     public void should_throw_an_exception_when_user_is_not_logged() {
-        TripService sut = new TestableTripService();
+        TripService sut = new TestableTripService(null);
         sut.getTripsByUser(null);
     }
 
+    @Test
+    public void should_return_any_trips_when_user_is_logged_and_are_not_friends() {
+        User aUserNotFriend = new User();
+
+        List<Trip> tripsByUser = new TestableTripService(new User()).getTripsByUser(aUserNotFriend);
+
+        assertThat(tripsByUser.size(), is(0));
+    }
+
     private class TestableTripService extends TripService {
+
+        private User loggedUserReturn;
+
+        public TestableTripService(User loggedUserReturn) {
+            this.loggedUserReturn = loggedUserReturn;
+        }
+
         @Override
         protected User obtainLoggedUser() {
-            return null;
+            return this.loggedUserReturn;
         }
     }
 }
